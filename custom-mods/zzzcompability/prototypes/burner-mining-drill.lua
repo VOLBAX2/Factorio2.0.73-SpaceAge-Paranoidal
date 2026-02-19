a27a570fdf390594adafa7dcfa24a6705909f855
Tias <timur.buchkin@mail.ru>
2026-02-20 01:53:56 +0300

    burner-mining-drill can get water now

--- custom-mods/zzzcompability/prototypes/burner-mining-drill.lua
+++ custom-mods/zzzcompability/prototypes/burner-mining-drill.lua
@@ -1,15 +1,37 @@
 -- add input fluid box for default burner drill
 
+
+
+---Creates sprite
+---@param filename string
+---@param icon_size number | number[] must be 2^n
+---@return SpritePrototype
+local function CreateSprite(filename, icon_size)
+	return {
+		filename=filename,
+		size=icon_size
+	}
+end
+
 ---@param volume number must be positive
 ---@param pipe_connections PipeConnectionDefinition[]
 ---@param direction "input" | "output"
 ---@return FluidBox
 local function CreateFluidBoxTemplate(volume, pipe_connections)
+	local directions = {"north", "east", "west", "south"}
+	local pipe_covers = {}
+	for _, d in ipairs(directions) do
+		pipe_covers[d] =
+				CreateSprite("__base__/graphics/entity/pipe-covers/pipe-cover-".. d..".png", 32)
+	end
 	return {
 		volume = volume,
 		pipe_connections = pipe_connections,
+		production_type="input",
+		pipe_covers = pipe_covers
 	}
 end
+
 
 ---@param bbox BoundingBox
 ---@param flow_direction FlowDirection
@@ -27,7 +49,7 @@ local function CreatePipeConnectionToCollisionBox(bbox, flow_direction, directio
 	anchor = {0.3, 0.3} -- debug
 	return {
 		flow_direction = flow_direction,
-		direction = direction or defines.direction.north,
+		direction = direction or defines.direction.south,
 		position = anchor,
 	}
 end
@@ -37,9 +59,26 @@ end
 ---@param volume? number (default: 1000) must be positive
 local function AddInputFluidBox(drill, volume)
 	local bbox = drill.collision_box
-	local pipe_connection = CreatePipeConnectionToCollisionBox(bbox, "input-output")
+	local pipe_connection = CreatePipeConnectionToCollisionBox(bbox, "input-output", defines.direction.south)
 
-	drill.input_fluid_box = drill.input_fluid_box or CreateFluidBoxTemplate(volume or 1000, { pipe_connection })
+
+	drill.input_fluid_box = CreateFluidBoxTemplate(volume or 1000, { pipe_connection })
+end
+
+---Set trigger to mine 10 coal and open mining-with-fluid modifier
+---@param techName string
+local function UnlockMiningWithFluidWithTech(techName)
+	local tech = data.raw.technology[techName]
+	assert(tech, "Given tech not found!")
+
+	tech.unit = nil
+	tech.research_trigger = {
+		type = "mine-entity",
+		entity="coal"
+	}
+	table.insert(tech.effects, {type="mining-with-fluid", modifier=true})
 end
 
 AddInputFluidBox(data.raw["mining-drill"]["burner-mining-drill"])
+UnlockMiningWithFluidWithTech("burner-mechanics")
+
